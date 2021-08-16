0e872bfc575f8ea899a2c6b36dd34ffa5e93cf84
younghoon <digitcomgisul@gmail.com>
2021-08-17 08:25:19 +0900

    Initial commit

---
+++ src/test/java/kr/digitcom/naverocr/NaverocrApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.digitcom.naverocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverocrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
